91d2c60b7b918248220c1ae2317d050d41b5d47b
AustinMcKee327 <47399911+AustinMcKee327@users.noreply.github.com>
2019-05-14 10:03:43 -0400

    Add files via upload

--- Tracks.cs
+++ Tracks.cs
@@ -20,80 +20,73 @@ namespace Sprint_2
 
     public class Tracks
     {
+        public Polygon TrackOneInsideOne = new Polygon();
+        Point InsideOne1 = new Point(300, 125);
+        Point InsideOne2 = new Point(900, 125);
+        Point InsideOne3 = new Point(1100, 325);
+        Point InsideOne4 = new Point(1100, 525);
+        Point InsideOne5 = new Point(900, 725);
+        Point InsideOne6 = new Point(300, 725);
+        Point InsideOne7 = new Point(100, 525);
+        Point InsideOne8 = new Point(100, 325);
+        PointCollection InsideOne = new PointCollection();
+       
         /// <summary>
         ///Tracks and their layouts
         /// </summary>
         Canvas canvas;
-        Rectangle wall1 = new Rectangle();
-        Rectangle wall2 = new Rectangle();
-        Rectangle wall3 = new Rectangle();
-        Rectangle wall4 = new Rectangle();
-
-        Point wall1Pos = new Point();
-        Point wall2Pos = new Point();
-        Point wall3Pos = new Point();
-        Point wall4Pos = new Point();
+        public Player player;
+       
         public void Track1(Canvas c)
         {
             canvas = c;
             
-            Polygon TrackOneInsideOne = new Polygon();
-            Point InsideOne1 = new Point(300,300);
-            Point InsideOne2 = new Point(900,300);
-            Point InsideOne3 = new Point(1100,500);
-            Point InsideOne4 = new Point(1100,700);
-            Point InsideOne5 = new Point(900, 900);
-            Point InsideOne6 = new Point(300, 900);
-            Point InsideOne7 = new Point(100, 700);
-            Point InsideOne8 = new Point(100, 500);
-            PointCollection InsideOne = new PointCollection();
-            InsideOne.Add(InsideOne1);
-            InsideOne.Add(InsideOne2);
-            InsideOne.Add(InsideOne3);
-            InsideOne.Add(InsideOne4);
-            InsideOne.Add(InsideOne5);
-            InsideOne.Add(InsideOne6);
-            InsideOne.Add(InsideOne7);
-            InsideOne.Add(InsideOne8);
-            TrackOneInsideOne.Points = InsideOne;
-            TrackOneInsideOne.Stroke = Brushes.Green;
-            TrackOneInsideOne.StrokeThickness = 200;
-            c.Children.Add(TrackOneInsideOne);
-            //Polygon TrackOneInsideTwo = new Polygon();
-            //Point InsideTwo1 = new Point(300, 400);
-            //Point InsideTwo2 = new Point(900, 400);
-            //Point InsideTwo3 = new Point(1000, 500);
-            //Point InsideTwo4 = new Point(1000, 700);
-            //Point InsideTwo5 = new Point(900, 800);
-            //Point InsideTwo6 = new Point();
-            //Point InsideTwo7 = new Point();
-            //Point InsideTwo8 = new Point();
-            //PointCollection InsideTwo = new PointCollection();
-            //InsideTwo.Add(InsideTwo1);
-            //InsideTwo.Add(InsideTwo2);
-            //InsideTwo.Add(InsideTwo3);
-            //InsideTwo.Add(InsideTwo4);
-            //InsideTwo.Add(InsideTwo5);
-            //InsideTwo.Add(InsideTwo6);
-            //InsideTwo.Add(InsideTwo7);
-            //InsideTwo.Add(InsideTwo8);
-            //TrackOneInsideTwo.Points = InsideTwo;
-            //TrackOneInsideTwo.Fill = Brushes.Blue;
-            //c.Children.Add(TrackOneInsideTwo);
+            
+
         }
         public void Track2(Canvas c)
         {
             canvas = c;
-            wall1.Height = 10;
-            wall1.Width = 100;
-            wall1Pos.Y = 300;
-            wall1Pos.X = 200;
-            wall1.Fill = Brushes.Aqua;
-            canvas.Children.Add(wall1);
-            Canvas.SetLeft(wall1, wall1Pos.X);
-            Canvas.SetTop(wall1, wall1Pos.Y);
+            Polygon inside = new Polygon();
+            Point TL = new Point(225, 150);
+            Point TR = new Point(375, 150);
+            Point MM = new Point(375, 525);
+            Point MR = new Point(775, 525);
+            Point BR = new Point(775, 725);
+            Point BL = new Point(225, 725);
+            PointCollection pointCollection = new PointCollection();
+            pointCollection.Add(TL);
+            pointCollection.Add(TR);
+            pointCollection.Add(MM);
+            pointCollection.Add(MR);
+            pointCollection.Add(BR);
+            pointCollection.Add(BL);
+            inside.Points = pointCollection;
+            inside.Fill = Brushes.Black;
             
+            Polygon outside = new Polygon();
+            Point OTL = new Point(100, 25);
+            Point OTR = new Point(500, 25);
+            Point OMM = new Point(500, 425);
+            Point OMR = new Point(900,425);
+            Point OBR = new Point(900,825);
+            Point OBL = new Point(100,825);
+            PointCollection collection = new PointCollection();
+            collection.Add(OTL);
+            collection.Add(OTR);
+            collection.Add(OMM);
+            collection.Add(OMR);
+            collection.Add(OBR);
+            collection.Add(OBL);
+            outside.Points = collection;
+            outside.Fill = Brushes.Purple;
+            canvas.Children.Add(outside);
+            canvas.Children.Add(inside);
+
+
         }
         public void Track3(Canvas c) { }
+
+        
     }
 }
