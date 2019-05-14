6a770dd188b0d68971a7f8b8a95e520a91440f89
AustinMcKee327 <47399911+AustinMcKee327@users.noreply.github.com>
2019-05-14 09:31:59 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Sprint_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    enum GameState { MainMenu,Loading,TrackSelection,Time,GameOn}
+    
+    public partial class MainWindow : Window
+    {
+        //globaldouble 
+        public DispatcherTimer gametimer = new DispatcherTimer();
+        Tracks t = new Tracks();
+        public static int trackNum;
+        GameState gameState = new GameState();
+        public Player p1 = new Player();
+        public static string angle ="";
+        public static double facing = 0;
+        public static double facingactual = 0;
+
+
+        public MainWindow()
+        {   
+            InitializeComponent();
+            gameState = GameState.MainMenu;
+            gametimer.Tick += Gametimer_Tick;
+            gametimer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            gametimer.Start();
+            
+        }
+
+        private void Gametimer_Tick(object sender, EventArgs e)
+        {
+            angle = facing.ToString();
+            lblcomputedangle.Content ="Computed Angle:" +facingactual.ToString();
+            lblangle.Content = "Angle :" +angle;
+            if (gameState == GameState.Loading)
+            {
+                if (Keyboard.IsKeyToggled(Key.Space))
+                {
+                    gameState = GameState.GameOn;
+                }
+            }
+            if (gameState == GameState.GameOn)
+            {
+                p1.update(canvas);
+                
+            }
+            if (Keyboard.IsKeyToggled(Key.Space))
+            {
+                gameState = GameState.Loading;
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            TrackSelection TS = new TrackSelection();
+            TS.ShowDialog();
+        }
+
+        private void startGame_Click(object sender, RoutedEventArgs e)
+        {
+            gameState = GameState.Loading;
+            switch (trackNum) //switches, allows default and used for testing because I'm lazy. I have to click 3 less buttons to start testing
+            {
+                case 1:
+                    gameState = GameState.Loading;
+                    selectTrack.Visibility = Visibility.Hidden;
+                    startGame.Visibility = Visibility.Hidden;
+                    t.Track1(canvas);
+                    p1.drawPlayer(canvas);
+                    break;
+                case 2:
+                    gameState = GameState.Loading;
+                    selectTrack.Visibility = Visibility.Hidden;
+                    startGame.Visibility = Visibility.Hidden;
+                    t.Track2(canvas);
+                    p1.drawPlayer(canvas);
+                    break;
+                case 3:
+                    gameState = GameState.Loading;
+                    selectTrack.Visibility = Visibility.Hidden;
+                    startGame.Visibility = Visibility.Hidden;
+                    p1.drawPlayer(canvas);
+                    t.Track3(canvas);
+                    break;
+                default:
+                    gameState = GameState.Loading;
+                    selectTrack.Visibility = Visibility.Hidden;
+                    startGame.Visibility = Visibility.Hidden;
+                    t.Track2(canvas);
+                    p1.drawPlayer(canvas);
+                    break;
+            }
+        }
+
+        private void Window_LostFocus(object sender, RoutedEventArgs e)
+        {
+            gameState = GameState.Loading;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,111 @@
+﻿///314243
+///5/3/19
+///Player class and wall detection
+///includes updating the player's location, angle and hit detection
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace Sprint_2
+{
+
+    public class Player
+    {
+        ImageBrush sprite;
+        BitmapImage bitmap = new BitmapImage(new Uri("Aqua-Car.png",UriKind.Relative));
+
+        //player globals
+        //Ethan Hunter
+        double ang = 0;   //displays the angle on game window if toggled on
+        double i = 0;     //used to change the direction the player is facing 
+        public double speed = 15; //constant for circle movement
+        public Point position = new Point(50,50); //position of car on screen
+        Rectangle car = new Rectangle();
+        Canvas canvas;
+        public double angle = 0; //secondary angle used in facing positions, used for radian to degree conversion
+        //double test = 90;
+
+        /// <summary>
+        /// Call player
+        /// </summary>
+        public Player()
+        { 
+            Point centre = new Point();      //centre of player     
+            Canvas.SetLeft(car, position.X); 
+            Canvas.SetTop(car, position.Y);
+            car.RenderTransformOrigin = new Point(0.5, 0.5);//centre of car
+            centre = car.RenderTransformOrigin; //appoints the centre of the car to our point of rotation
+        }
+        
+        /// <summary>
+        /// Recreates the player, includes movement and rotation based on keys pressed
+        /// </summary>
+        public void update(Canvas c)
+        {
+            angle = (Math.PI*i/180);//sin and cos work in RAD, this is the conversion           
+            canvas = c;
+            if (Keyboard.IsKeyDown(Key.Up))
+            {
+                position.Y -= speed*(Math.Cos(angle)); //goes up if facing up (cos0 =1), no change if facing right/left (cos90/270)
+                position.X += speed*(Math.Sin(angle)); //goes right if facing right, no change if facing up
+                Canvas.SetTop(car, position.Y);
+                Canvas.SetLeft(car, position.X);
+            }
+            if (Keyboard.IsKeyDown(Key.Down))
+            {
+                position.Y += speed*(Math.Cos(angle)); //goes down if facing up, no change if facing right/left
+                position.X -= speed*(Math.Sin(angle)); //goes left if facing right, no change if up
+                Canvas.SetTop(car, position.Y);
+                Canvas.SetLeft(car, position.X);
+            }
+            if (Keyboard.IsKeyDown(Key.Right)) //checks for rotation clockwise
+            {
+                i += 57.2958/6;  //turn speed is 1 radian/constant
+                angle = i;       //used in displaying the angle, and movement speed
+                if(angle > 360) { angle -= 360; } //resets if over 360degrees
+                ang = i;
+            }
+            if (Keyboard.IsKeyDown(Key.Left)) //checks rotation counter-clockwise
+            {
+                i -= 57.2958/6;   //rotation speed
+                angle = i;
+                if(angle <  0) { angle += 360; }               
+                ang = i;             
+            }
+            MainWindow.facing = ang;
+            MainWindow.facingactual = angle;
+            car.RenderTransform = new RotateTransform() { Angle = i};
+
+
+
+        }
+        public void drawPlayer(Canvas c)
+        {
+            sprite = new ImageBrush(bitmap);         //sprite/character model
+            sprite.Stretch = Stretch.None;          
+            sprite.AlignmentX = AlignmentX.Left;
+            sprite.AlignmentY = AlignmentY.Top;
+            sprite.Viewport = new Rect(0, 0, 100, 100);
+            //car.Fill = sprite;
+            car.Fill = Brushes.Aqua;                  //solid model instead of sprite
+            canvas = c;
+            car.Width = 70;
+            car.Height = 70;
+            c.Children.Add(car); //puts character on canvas
+           
+        }
+
+    }
+}
